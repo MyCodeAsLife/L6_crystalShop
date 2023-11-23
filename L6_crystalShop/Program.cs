e7169c6373fc2b5f5fb1cedab89222d771a259e6
Vladimir <w112vovan@mail.ru>
2023-11-23 10:51:33 +0500

    Добавьте файлы проекта.

---
+++ L6_crystalShop/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L6_crystalShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int pricePerCrystal = random.Next(10, 16);
+            int userMoney = 0;
+            int userCrystals = 0;
+            int desiredCountOfCrystals = 0;
+
+            Console.Write("Сколько у вас монет?: ");
+            userMoney = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Приветствуем в нашем магазине.\nСегодня цена за кристалл составляет: {pricePerCrystal}\n");
+            Console.Write("Сколько кристаллов вы желаете приобрести?: ");
+            desiredCountOfCrystals = Convert.ToInt32(Console.ReadLine());
+
+            userMoney -= desiredCountOfCrystals * pricePerCrystal;
+            userCrystals = desiredCountOfCrystals;
+
+            Console.WriteLine($"\nУ вас монет: {userMoney}, кристаллов: {userCrystals}\n");
+        }
+    }
+}
